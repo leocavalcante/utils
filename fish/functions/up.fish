b15df41bce68cf1f2d4596c1724808e06f70835c
Leo Cavalcante <lc@leocavalcante.com>
2017-02-08 14:20:48 -0200

    Use Docker composer if there is a file with the given port

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,7 +1,14 @@
-function up --description "Starts a php:apache Docker container using the current path on a random port"
-    set -l port (shuf -i 8000-8099 -n 1)
-    set -l cdir (pwd | string replace 'cygdrive' '')
+function up --description "Starts a php:apache Docker container using the current path on a random port or the Docker compose"
     subl .
-    cygstart http://docker:{$port}
-    docker run --rm -v $cdir:/var/www/html -p {$port}:80 php:apache
+
+    if test -e ./docker-compose.yml
+        cygstart http://docker:{$argv}
+        docker-compose up
+    else
+        set -l cdir (pwd | string replace 'cygdrive' '')
+        set -l port (shuf -i 8000-8099 -n 1)
+
+        cygstart http://docker:{$port}
+        docker run --rm -v {$cdir}:/var/www/html -p {$port}:80 php:apache
+    end
 end
