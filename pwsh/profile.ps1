20c817dea7c2c987c6d2d0b2c40aef3071ef39a5
Leo Cavalcante <lc@leocavalcante.com>
2019-12-13 19:12:24 -0300

    Update profile.ps1

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -8,7 +8,7 @@ function du {
     @args
 }
 
-function prompt {
+function Prompt {
   $p = Split-Path -Leaf -Path (Get-Location)
   $b = git rev-parse --abbrev-ref HEAD
 
@@ -17,6 +17,33 @@ function prompt {
   }
 
   return "$p~ "
+}
+
+# https://gist.github.com/grenzi/82e6cb8215cc47879fdf3a8a4768ec09
+function Set-PsEnv {
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Low')]
+    param($localEnvFile = ".\.env")
+
+    #return if no env file
+    if (!( Test-Path $localEnvFile)) {
+        Throw "could not open $localEnvFile"
+    }
+
+    #read the local env file
+    $content = Get-Content $localEnvFile -ErrorAction Stop
+    Write-Verbose "Parsed .env file"
+
+    #load the content to environment
+    foreach ($line in $content) {
+        if ($line.StartsWith("#")) { continue };
+        if ($line.Trim()) {
+            $line = $line.Replace("`"","")
+            $kvp = $line -split "=",2
+            if ($PSCmdlet.ShouldProcess("$($kvp[0])", "set value $($kvp[1])")) {
+                [Environment]::SetEnvironmentVariable($kvp[0].Trim(), $kvp[1].Trim(), "Process") | Out-Null
+            }
+        }
+    }
 }
 
 # cmd /c "vcvars64.bat&set" |
