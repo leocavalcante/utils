eff9bb6f62edd7a6b5f314629ef66b5cae43c267
Leo Cavalcante <lc@leocavalcante.com>
2020-07-09 11:42:11 -0300

    Update cup.ps1

--- pwsh/cup.ps1
+++ pwsh/cup.ps1
@@ -1,5 +1,21 @@
-choco upgrade -y chocolatey php nodejs-lts python ruby golang rustup.install dart-sdk meteor
+# Upgrade Chocolatey itself
+choco upgrade -y chocolatey
+
+# Upgrade PHP and Composer
+choco upgrade -y php
 composer self-update
-rustup update
+
+# Upgrade Node.js and NPM
+choco upgrade -y nodejs-lts
 npm update -g npm
+
+# Upgrade Dart and Flutter
+choco upgrade -y dart-sdk
 flutter upgrade
+
+# Upgrade Rust
+choco upgrade -y rustup.install
+rustup update
+
+# Upgrade Others
+choco upgrade -y python ruby golang
